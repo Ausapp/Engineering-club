cb6f87608a8428ab167276bb774fc2c485b6a6ba
Ausapp <111911210+Ausapp@users.noreply.github.com>
2024-11-11 10:58:11 -0500

    Add files via upload

--- sort.js
+++ sort.js
@@ -1,22 +1,50 @@
 var dropdownContent = document.querySelector(".dropdownContent");
 var dropButton = document.querySelector(".dropButton");
+var items = document.getElementsByClassName("item");
+everything();
+function everything() {
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.add("visible");
+    }
+}
 
-console.log(dropButton)
 function showDropdown() {
-    console.log("hello");
-    console.log(dropdownContent)
     dropdownContent.classList.toggle("visible");
 }
 
 window.addEventListener("click", function(e) {
-    console.log(e.target);
-    if(e.target != dropButton) {
-        console.log("bang");
+    if (e.target != dropButton) {
         dropdownContent.classList.remove("visible");
     }
-})
+});
+
+var names = {
+    everything: "Everything",
+    prints:"3d Prints",
+    leds:"Led Signs",
+    ornaments:"Ornaments",
+    other: "Other",
+}
 
 function sortItems(sortMethod) {
-    dropButton.innerHTML = sortMethod;
+    dropButton.innerHTML = `${names[sortMethod]} <i class="arrow down"></i>`;
     dropdownContent.classList.remove("visible");
+    
+    let sortItems = [];
+    if (sortMethod !== 'everything') {
+        sortItems = Array.from(document.getElementsByClassName(sortMethod));
+        
+
+        // Loop through all items and toggle the "visible" class based on sorting
+        for (let i = 0; i < items.length; i++) {
+            if (sortItems.includes(items[i])) {
+                items[i].classList.add("visible");
+            } else {
+                items[i].classList.remove("visible");
+            }
+        }
+    }else {
+        everything();
+    }
 }
+
--- sumbit.js
+++ sumbit.js
@@ -4,6 +4,7 @@
 
         // Collect form data into a JSON object
         const formData = {
+            type: 'contact',
             name: document.getElementById("name").value,
             email: document.getElementById("email").value,
             subject: document.getElementById("subject").value,
